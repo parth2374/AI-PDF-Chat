536227e295eec6524ed967100d746f136a0429a9
parth2374 <143696865+parth2374@users.noreply.github.com>
2024-11-04 20:46:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,14 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain_community.vectorstores import FAISS
+from langchain.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
 from dotenv import load_dotenv
 
 load_dotenv()
-os.getenv("GOOGLE_API_KEY")
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key='AIzaSyDJpHDY1JzGDvS1zYl0RZ7lt5YeSHHQmnA')
 
 def get_pdf_text(pdf_docs):
     text=""
